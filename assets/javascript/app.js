9898c642eaa727b1dbc025c19c5e75853f57f9f7
ANS <36783010+answebdev@users.noreply.github.com>
2021-01-05 21:32:43 -0800

    updated code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Using the Object.keys() method to turn an object into an array.
+// Convert an object into an array.
 
 // The Object.keys() method returns an array of a given object's own enumerable property names,
 // iterated in the same order that a normal loop would.
@@ -9,15 +9,15 @@
 
 // Object:
 const items = {
-  eggs: 1,
+  eggs: 12,
   apples: 5,
-  chocolate: 2,
   milk: 1,
-  almonds: 25,
 };
 
 console.log('\n');
 console.log(items); // Returns object
+
+console.log('\n');
 
 console.log(Object.keys(items)); // Returns an array of the keys
 console.log(Object.values(items)); // Returns an array of the values
--- assets/javascript/coalesce.js
+++ assets/javascript/coalesce.js
@@ -6,7 +6,7 @@
 // to find the first value that is not equal to undefined or null.
 
 const coalesceRomance = (...args) =>
-  args.find((v) => ![undefined, null].includes(v));
+    args.find((v) => ![undefined, null].includes(v));
 
 console.log(coalesceRomance(null, undefined, 'soulmate', NaN));
 
